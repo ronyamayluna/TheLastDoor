857dcf6a5e06610ba26fff968a1ccb4f209db215
Балкия Рания <Balkiya@corp.biom3>
2026-03-14 18:29:27 +0500

    Add Door opening code

---
+++ Assets/_SCRIPTS/Core/PickUpObjects.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class PickUpObjects : MonoBehaviour
+{
+    
+}
---
+++ Assets/_SCRIPTS/Items/CameraOpenDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraDoorScript
+{
+	public class CameraOpenDoor : MonoBehaviour
+	{
+		public float DistanceOpen = 3;
+		void Update()
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, transform.forward, out hit, DistanceOpen))
+			{
+				if (hit.transform.GetComponent<DoorScript.Door>())
+				{
+					if (Input.GetKeyDown(KeyCode.E))
+						hit.transform.GetComponent<DoorScript.Door>().OpenDoor();
+				}
+			}
+		}
+	}
+}
+
